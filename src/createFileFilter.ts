0790d1bef4766b17b2dc8303786ca3b4a8e6eb1f
Kei Ito <kei.itof@gmail.com>
2020-09-05 00:12:00 +0900

    feat: add createFileFilter

--- src/createFileFilter.ts
+++ src/createFileFilter.ts
@@ -1,23 +1,29 @@
+#!/usr/bin/env node
 import * as path from 'path';
+import {isMatch} from 'picomatch';
 
 export interface FileFilter {
     (file: string): boolean,
 }
 
+export const extendPatterns = (...patterns: Array<string>): Array<string> => patterns
+.map((pattern) => pattern.includes('/') ? pattern : `**/${pattern}`);
+
 export const createFileFilter = (
     {
-        extensions,
+        ext: extensions = [],
+        include = [],
+        exclude = [],
     }: {
-        extensions?: Array<string>,
-    } = {},
-): FileFilter => (
-    file: string,
-): boolean => {
-    if (!file) {
-        return false;
-    }
-    if (extensions && !extensions.includes(path.extname(file))) {
-        return false;
-    }
-    return true;
+        ext?: Array<string>,
+        include?: Array<string>,
+        exclude?: Array<string>,
+    },
+): FileFilter => {
+    const includePattern = extendPatterns(...extensions.map((ext) => `*${ext}`), ...include);
+    const excludePattern = extendPatterns(...exclude);
+    return (file) => {
+        const normalized = file.split(path.sep).join('/');
+        return isMatch(normalized, includePattern) && !isMatch(normalized, excludePattern);
+    };
 };
