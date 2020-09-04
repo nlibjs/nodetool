2f98f96094c240e5978405d9234e8189d6172607
Kei Ito <kei.itof@gmail.com>
2020-09-04 12:43:43 +0900

    feat: add indexen CLI

--- src/indexen.ts
+++ src/indexen.ts
@@ -2,16 +2,17 @@ import * as path from 'path';
 import {listFiles} from './listFiles';
 
 export const indexen = async function* (
-    {directory, include}: {
+    {directory, quote, include}: {
         directory: string,
+        quote: '\'' | '"' | '`',
         include: (file: string) => boolean,
     },
 ): AsyncGenerator<string> {
     for await (const file of listFiles(directory)) {
         if (include(file)) {
-            yield `export * from './`;
+            yield `export * from ${quote}./`;
             yield path.relative(directory, file).slice(0, -path.extname(file).length);
-            yield '\';\n';
+            yield `${quote};\n`;
         }
     }
     yield '\n';
